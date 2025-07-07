7e5911d16523ae6c6a792ace0b6380b94080eecd
student-demo <patelsamit968@gmail.com>
2025-07-08 00:17:48 +0530

    Add authentication and upload middleware

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+import jwt  from "jsonwebtoken";
+import User from "../models/User.js";
+
+ export const protect = async (req, res, next) => {
+    try {
+        let token = req.headers.authorization;
+
+        if(token && token.startsWith('Bearer')){
+            token = token.split(" ")[1];//extract token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.id).select("-password");
+            next();
+        }else {
+            res.status(401).json({message: "Not authorized, no token"});
+        }
+    }catch(error){
+        res.status(401).json({message: "Token failed", error: error.message});
+    }
+};
+
