e1790b005824ca3197e3f581aa2a9868a35a70d8
student-demo <patelsamit968@gmail.com>
2025-07-08 00:18:11 +0530

    Add Question model with schema definition

---
+++ backend/models/Question.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const questionSchema = new mongoose.Schema({
+    session: {type: mongoose.Schema.Types.ObjectId, ref: "Session"},
+    question: String,
+    answer: String,
+    note: String,
+    isPinned: {type: Boolean, default: false},
+}, {timestamps: true});
+
+const Question= mongoose.model("Question", questionSchema);
+export default Question;
